8df8b3720803741b5c58216496deee4256e9644f
cainm <cainm@rebel>
2011-11-12 20:08:16 -0500

    Build version info files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 21 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 SMP PREEMPT Sat Nov 12 17:10:38 EST 2011"
-#define LINUX_COMPILE_TIME "17:10:38"
+#define UTS_VERSION "#21 SMP PREEMPT Sat Nov 12 19:47:33 EST 2011"
+#define LINUX_COMPILE_TIME "19:47:33"
 #define LINUX_COMPILE_BY "cainm"
 #define LINUX_COMPILE_HOST "rebel"
 #define LINUX_COMPILE_DOMAIN "(none)"
